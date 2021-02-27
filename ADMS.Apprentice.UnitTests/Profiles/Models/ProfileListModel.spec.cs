5f10d2cf6351512b2f91d170ba99c936ba3af984
Raheena Abubacker <Raheena.Abubacker@dese.gov.au>
2021-02-27 16:12:42 +1100

    Added unit tests and removed old projects and refereces

---
+++ ADMS.Apprentice.UnitTests/Profiles/Models/ProfileListModel.spec.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ADMS.Apprentice.Core.Entities;
+using ADMS.Apprentice.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADMS.Apprentice.UnitTests.Profiles.Models
+{
+    #region WhenInstantiatingAProfileListModel
+
+    [TestClass]
+    public class WhenInstantiatingAProfileListModel : GivenWhenThen
+    {
+        private ProfileListModel model;
+        private Profile profile;
+
+        protected override void Given()
+        {
+            profile = new Profile
+            {
+                Id = 123,
+                FirstName = "Bob",
+                Surname = "Alex",
+                OtherNames = "Charlie",
+                BirthDate = DateTime.Now.AddYears(-25),
+                //GenderCode = "X",
+                //ProfileTypeCode = "Apprentice",
+                CreatedOn = DateTime.Now.AddMinutes(-3),            
+                CreatedBy = "User1",
+                UpdatedOn = DateTime.Now,
+                UpdatedBy = "User2",
+            };
+        }
+
+        protected override void When()
+        {
+            model = new ProfileListModel(profile);
+        }
+
+        [TestMethod]
+        public void ReturnsAModel()
+        {
+            model.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void SetsPropertiesOnModel()
+        {
+            model.Id.Should().Be(123);
+            model.FirstName.Should().Be("Bob");
+            model.Surname.Should().Be("Alex");
+            model.OtherNames.Should().Be("Charlie");
+            model.BirthDate.Should().BeCloseTo(DateTime.Now.AddYears(-25));
+            model.CreatedOn.Should().BeCloseTo(DateTime.Now.AddMinutes(-3));            
+            model.UpdatedOn.Should().BeCloseTo(DateTime.Now);
+            model.CreatedBy.Should().Be("User1");
+            model.UpdatedBy.Should().Be("User2");
+        }
+    }
+    #endregion
+}
---
+++ ADMS.Apprentice.UnitTests/Profiles/Services/ProfileCreator.spec.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ADMS.Apprentice.Core.Entities;
+using ADMS.Apprentice.Core.Messages;
+using ADMS.Apprentice.Core.Services;
+using Adms.Shared;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADMS.Apprentice.UnitTests.Profiles.Services
+{
+    #region WhenCreatingAProfile
+    [TestClass]
+    public class WhenCreatingAProfile : GivenWhenThen<ProfileCreator>
+    {
+        private const int registrationId = 123;
+        private Profile profile;
+        private ProfileMessage message;
+
+        protected override void Given()
+        {
+            message = new ProfileMessage
+            {
+                Surname = "Bob",
+                FirstName = "Alex",
+                BirthDate = DateTime.Now.AddYears(-25)
+            };
+        }
+
+        protected override async void When()
+        {
+            profile = await ClassUnderTest.CreateAsync(message);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProfile()
+        {
+            profile.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void ShouldAddTheProfileToTheDatabase()
+        {
+            Container.GetMock<IRepository>().Verify(r => r.Insert(profile));
+        }
+
+        [TestMethod]
+        public void ShouldSetTheName()
+        {
+            profile.FirstName.Should().Be(message.FirstName);
+            profile.Surname.Should().Be(message.Surname);
+        }
+
+        [TestMethod]
+        public void ShouldSetTheBirthDate()
+        {
+            profile.BirthDate.Should().Be(message.BirthDate);
+        }
+
+        [TestMethod]
+        public void ShouldSetDefaultValues()
+        {
+            profile.DeceasedFlag.Should().BeFalse();
+            profile.ActiveFlag.Should().BeTrue();            
+        }
+
+        [TestMethod]
+        public void ShouldSetProfileType()
+        {
+            profile.ProfileTypeCode.Should().Be(ProfileType.Apprentice.ToString());
+        }
+
+        //[TestMethod]
+        //public void ShouldSetCreatedAndLastModifiedDates()
+        //{
+        //    profile.CreatedOn.Should().BeCloseTo(DateTime.Now);
+        //    profile.UpdatedOn.Should().BeCloseTo(DateTime.Now);
+        //}
+    }
+
+    #endregion
+}
