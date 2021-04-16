a03607ac2ec7e3f53f2a21bb549bc71eed2257b0
Raheena Abubacker <Raheena.Abubacker@dese.gov.au>
2021-04-16 16:26:20 +1000

    Added profile updated

---
+++ ADMS.Apprentice.Core/Services/ProfileUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using ADMS.Apprentice.Core.Entities;
+using ADMS.Apprentice.Core.Messages;
+using Adms.Shared.Attributes;
+
+namespace ADMS.Apprentice.Core.Services
+{
+    [RegisterWithIocContainer]
+    public class ProfileUpdater : IProfileUpdater
+    {
+        private readonly IProfileUpdater profileUpdater;
+        public ProfileUpdater(IProfileUpdater profileUpdater)
+        {
+            this.profileUpdater = profileUpdater;
+        }
+
+        public void Update(Profile profile, ProfileMessage message)
+        { }
+    }
+}
