82be9d414b9d7da2f6700c136bd06fc5cad5a8f9
John Donato <John.Donato@jobs.gov.au>
2021-06-01 04:51:05 +0000

    Merged PR 1998: Add StateCode to Address Full Text Index
    add statecode
    
    Related work items: #100469

--- Database.ADMSApprentice/FullTextIndexes.sql
+++ Database.ADMSApprentice/FullTextIndexes.sql
@@ -6,7 +6,7 @@
 
 GO
 CREATE FULLTEXT INDEX ON [dbo].[ApprenticeAddress]
-    ([StreetAddress1] LANGUAGE 1033, [StreetAddress2] LANGUAGE 1033, [StreetAddress3] LANGUAGE 1033, [Locality] LANGUAGE 1033, [Postcode] LANGUAGE 1033)
+    ([StreetAddress1] LANGUAGE 1033, [StreetAddress2] LANGUAGE 1033, [StreetAddress3] LANGUAGE 1033, [Locality] LANGUAGE 1033, [Postcode] LANGUAGE 1033, [StateCode] LANGUAGE 1033)
     KEY INDEX [PK_ApprenticeAddress]
     ON [FT_Apprentice];
 
