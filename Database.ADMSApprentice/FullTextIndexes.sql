54cec98058ccf5849df79a748202a991a17d47a8
John Donato <John.Donato@jobs.gov.au>
2021-06-22 03:51:20 +0000

    Merged PR 2610: Refine ApprenticeAddress full text index with stopwords
    Refine ApprenticeAddress full text index with stopwords
    
    Related work items: #109253

--- Database.ADMSApprentice/FullTextIndexes.sql
+++ Database.ADMSApprentice/FullTextIndexes.sql
@@ -6,7 +6,10 @@
 
 GO
 CREATE FULLTEXT INDEX ON [dbo].[ApprenticeAddress]
-    ([StreetAddress1] LANGUAGE 1033, [StreetAddress2] LANGUAGE 1033, [StreetAddress3] LANGUAGE 1033, [Locality] LANGUAGE 1033, [Postcode] LANGUAGE 1033, [StateCode] LANGUAGE 1033)
+    ([StreetAddress1] LANGUAGE 1033, [StreetAddress2] LANGUAGE 1033, [StreetAddress3] LANGUAGE 1033, [Locality] LANGUAGE 1033, [Postcode] LANGUAGE 1033)
     KEY INDEX [PK_ApprenticeAddress]
-    ON [FT_Apprentice];
+    ON [FT_Apprentice]
+    WITH STOPLIST [AddressStoplist];
+
+
 
---
+++ Database.ADMSApprentice/Storage/AddressStoplist.sql
@@ -0,0 +1,3 @@
+﻿CREATE FULLTEXT STOPLIST [AddressStoplist]
+    AUTHORIZATION [dbo];
+
