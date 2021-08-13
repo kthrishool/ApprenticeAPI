84809a943fc51bd68dac40e0442bb29b12873c97
Mathew Frank <Mathew.Frank@dese.gov.au>
2021-08-13 04:13:58 +0000

    Merged PR 4393: Rename to table PriorQualifications, and new table PriorAprenticeships
    Rename to table PriorQualifications, and new table PriorAprenticeships
    
    Related work items: #126637, #126645

--- Database.ADMSApprentice/dbo/Tables/PriorQualification.sql
+++ Database.ADMSApprentice/dbo/Tables/PriorQualification.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE [dbo].[ApprenticeQualification] (
-    [ApprenticeQualificationId] INT             IDENTITY (1, 1) NOT NULL,
+﻿CREATE TABLE [dbo].[PriorQualification] (
+    [PriorQualificationId] INT             IDENTITY (1, 1) NOT NULL,
     [ApprenticeId]              INT             NOT NULL,
     [ApprenticeshipId]          INT             NULL,
     [QualificationCode]         VARCHAR (10)    NOT NULL,
@@ -14,9 +14,9 @@
     [UpdatedOn]                 DATETIME2 (7)   NOT NULL,
     [Version]                   ROWVERSION      NOT NULL,
     [_AuditEventId]             BIGINT          NOT NULL,
-    CONSTRAINT [PK_ApprenticeQualification] PRIMARY KEY CLUSTERED ([ApprenticeQualificationId] ASC),
-    CONSTRAINT [FK_ApprenticeQualification_Apprentice] FOREIGN KEY ([ApprenticeId]) REFERENCES [dbo].[Apprentice] ([ApprenticeId])
+    CONSTRAINT [PK_PriorQualification] PRIMARY KEY CLUSTERED ([PriorQualificationId] ASC),
+    CONSTRAINT [FK_PriorQualification_Apprentice] FOREIGN KEY ([ApprenticeId]) REFERENCES [dbo].[Apprentice] ([ApprenticeId])
 );
 GO
-CREATE NONCLUSTERED INDEX [IX_ApprenticeQualification_ApprenticeId]
-    ON [dbo].[ApprenticeQualification]([ApprenticeId] ASC) WITH (FILLFACTOR = 90);
+CREATE NONCLUSTERED INDEX [IX_PriorQualification_ApprenticeId]
+    ON [dbo].[PriorQualification]([ApprenticeId] ASC) WITH (FILLFACTOR = 90);
